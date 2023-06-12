22cf7d0f1dff525fdf77903ca0668abf4d47f0a5
ParkJuseung <judang0322@gmail.com>
2023-06-12 18:18:05 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import {Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
+import data from './data.js'
+
 
 function App() {
+
+  let [shoes] = useState()
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Shoes Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -16,9 +22,26 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg'>
-        
-      </div>
+      <div className='main-bg'/>
+      <Container>
+      <Row>
+        <Col sm>
+          <img src= "https://codingapple1.github.io/shop/shoes1.jpg" width = "80%"/>
+          <h4>{data[0].title}</h4>
+          <p>{data[0].content}</p>
+        </Col>
+        <Col sm>
+          <img src= "https://codingapple1.github.io/shop/shoes2.jpg" width = "80%"/>
+          <h4>{data[1].title}</h4>
+          <p>{data[1].content}</p>
+        </Col>
+        <Col sm>
+          <img src= "https://codingapple1.github.io/shop/shoes3.jpg" width = "80%"/>
+          <h4>{data[2].title}</h4>
+          <p>{data[2].content}</p>
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
