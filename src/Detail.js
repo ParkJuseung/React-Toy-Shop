f1da3e95c64d9fde05f2c78786f306acb1eb9c4a
ParkJuseung <judang0322@gmail.com>
2023-06-18 19:43:18 +0900

    useEffect, lifecycle

--- src/Detail.js
+++ src/Detail.js
@@ -1,11 +1,23 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 function Detail(props) {
     let {id} = useParams();
+    let [alert, setalert]= useState(true);
   
+    useEffect(() => {
+      setTimeout(() => {setalert(false)}, 2000);
+    })
     return (
       <div className="container">
+        {
+          alert == true 
+          ? <div className="alert alert-warning">
+          2초 이내 구매시 할인
+        </div>
+          : null
+        }
       <div className="row">
         <div className="col-md-6">
           <img src={props.shoes[id].img} width="100%" />
